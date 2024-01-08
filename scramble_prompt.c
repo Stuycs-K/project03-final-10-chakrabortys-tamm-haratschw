ef30077e0348a6faa3ba63b90cc6ab389784c18e
Matthew Tam <mtam40@stuy.edu>
2024-01-08 15:33:07 -0500

    Random character replacement complete

--- scramble_prompt.c
+++ scramble_prompt.c
@@ -4,11 +4,19 @@
 #include <stdlib.h>
 
 char* scramble(char* string){
-    char* modSTR = string;
+    char* list = calloc(strlen(string), sizeof(list));
+    char* modSTR = list;
+    int i = 0;
+    for(i = 0; i < strlen(string); i++){
+        list[i] = *(string + i);
+    }
+    list[i] = "\0";
     //while(modSTR < start + strlen(string)){}
-    printf("Character to be modified: %s\n", modSTR + 4);
-    *modSTR = 65;//rand() % 94 + 32;
-    printf("%s", string);
+    printf("Character to be modified: %d or %c\n", *modSTR, *modSTR);
+    srand(time(NULL));
+    *modSTR = rand() % 94 + 32;
+    printf("Character modified into: %d or %c\n", *modSTR, *modSTR);
+    printf("%s\n", list);
     return string;
 }
 
